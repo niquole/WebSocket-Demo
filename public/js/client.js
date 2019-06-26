835fe1c4d5362047ab7cb89e36b11e4e52e4741e
niquole <nicole.antonopoulou@gmail.com>
2019-06-26 11:23:12 +0200

    done

--- public/js/client.js
+++ public/js/client.js
@@ -1,23 +1,23 @@
-var socket = new WebSocket('ws://localhost:3000');
+// var socket = new WebSocket('ws://localhost:3000');
 
-socket.addEventListener('open', function() {
-    console.log("Connected to Websocket");
+// socket.addEventListener('open', function() {
+//     console.log("Connected to Websocket");
 
-})
-socket.addEventListener('close', function(){
-    console.log('Disconnected from Websocket');
-})
+// })
+// socket.addEventListener('close', function(){
+//     console.log('Disconnected from Websocket');
+// })
 
-socket.addEventListener('message', function(message){
-    console.log('Message from the server: ', message);
-})
+// socket.addEventListener('message', function(message){
+//     console.log('Message from the server: ', message);
+// })
 
-socket.addEventListener('error', function(error){
-    console.log(error);
-})
+// socket.addEventListener('error', function(error){
+//     console.log(error);
+// })
 
-document.querySelector('button').addEventListener('click', function() {
-    var text = document.querySelector('input').value;
-    console.log(text);
-    socket.send(text);
-})
+// document.querySelector('button').addEventListener('click', function() {
+//     // var text = document.querySelector('input').value;
+//     console.log(text);
+//     socket.send(text);
+// })
--- server.js
+++ server.js
@@ -5,7 +5,6 @@ server.on('connection', function(client){
     console.log(client + ' has connected');
 
     client.on('message', function(message){
-        console.log(client + ': ' + message);
         client.send(message);
     })
     
